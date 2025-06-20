099f1e622e58fc84b3f5834a943d6192c9dedfbd
Azxxzak <azjargal9524@gmail.com>
2025-06-20 17:54:29 +0800

    az

--- task-manager/vue.config.js
+++ task-manager/vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/task-manager/' : '/',
+  publicPath: '/',
   outputDir: 'dist',
   transpileDependencies: true
 })
